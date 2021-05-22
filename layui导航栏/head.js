8d882ed28e4eeb38269268c843acea5854751d43
msgithub-hub <2095358314@qq.com>
2021-05-22 16:52:00 +0800

    layui的table功能

--- layui导航栏/head.js
+++ layui导航栏/head.js
@@ -1,21 +1,24 @@
-layui.use( 'jquery', () => {
+layui.use('jquery', () => {
     let element = layui.element;
-    let $=layui.jquery;
+    let $ = layui.jquery;
     // let layer=layui.layer;
     element.render();
     $(function () {
         // 媒体查询
-        $('.head').on('click', 'h2',function () {
+        $('.head').on('click', 'h2', function () {
             $('.layui-nav-tree1').show(500);
         });
-        $('.head').on('click', '.close',function () {
+        $('.head').on('click', '.close', function () {
             $('.layui-nav-tree1').hide(500);
         });
 
-        $('.ulFa li a').each(function(){
-            if($($(this))[0].href==String(window.location))
-                $(this).parent().addClass('layui-this').siblings('.layui-this').removeClass('layui-this');
+        $('.ulFa li a').each(function () {
+            if (this.href == String(window.location)) {
+                $(this).parent().addClass('layui-this').siblings('.ulFa li a').removeClass('layui-this');
+                // $(this).addClass('layui-this').siblings('.ulFa li ').removeClass('layui-this');
+            }
         });
+
 
     })
 
